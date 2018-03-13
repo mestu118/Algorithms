4ba4938b642f2e9c71b113a3ed21df76dd54fc19
Mark Estudillo <markestu@buffalo.edu>
2018-03-13 10:13:09 -0400

    Initialized Week4 assigment - Puzzle 8

---
+++ Week4/src/Board.java
@@ -0,0 +1,82 @@
+
+public class Board {
+	
+	/**
+	 *  Constructs a board from an n-by-n array blocks
+	 *  where blocks[i][j] = block in row i, column j
+	 * @param blocks n-by-n array of blocks
+	 */
+	public Board(int[][] blocks) {
+		
+	}
+	
+	/**
+	 * Board dimension n
+	 * @return n Dimension of board
+	 */
+	public int dimension() {
+		return 0; 
+	}
+	
+	/**
+	 * Number of blocks out of place
+	 * @return int number of blocks out of place 
+	 */
+	public int hammin() {
+		return 0; 
+	}
+	
+	/**
+	 * Sum of Manhattan distance between blocks and goal
+	 * @return Manhattan distance 
+	 */
+	public int manhattan() {
+		return 0; 
+	}
+	
+	/**
+	 * Is this board the goal board? 
+	 * @return boolean to check if Goal board
+	 */
+	public boolean isGoal() {
+		return false; 
+	}
+	
+	/**
+	 * A board that is obtained by exchanging any pair of blocks
+	 * @return Board
+	 */
+	public Board twin() {
+		return null;
+	}
+	
+	/**
+	 * Does the board equal y
+	 * @param y compare to 
+	 * @return boolean is it equal to y
+	 */
+	public boolean equal(Object y) {
+		return false; 
+	}
+	
+	/**
+	 * All neighboring board
+	 * @return Iterable for neighboring board 
+	 */
+	public Iterable<Board> neighbors() {
+		return null; 
+	}
+	
+	/**
+	 * String that represents the board 
+	 * @return String 
+	 */
+	public String toString() {
+		return ""; 
+	}
+	
+	public static void main(String[] main) {
+		// Will modify as necessary 
+	}
+
+}
---
+++ Week4/src/Solver.java
@@ -0,0 +1,23 @@
+
+public class Solver {
+	public Solver(Board initials) {
+		
+	}
+	
+	public boolean isSolvable() {
+		return false;
+	}
+	
+	public int move() {
+		return 0; 
+	}
+	
+	public Iterable<Board> solution() {
+		return null; 
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
